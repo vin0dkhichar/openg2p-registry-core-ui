3dbbdfc80b8ccbc9ccf8a71d7b05f1b7321a3e66
vin0dkhichar <abbb61667@gmail.com>
2026-05-13 17:22:15 +0530

    remove csp

--- next.config.ts
+++ next.config.ts
@@ -5,39 +5,10 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
 const isDev = process.env.NODE_ENV === 'development';
-const connectSrc = process.env.CSP_CONNECT_SRC || "'self'";
-
-const cspHeader = `
-    default-src 'self';
-    script-src 'self' 'unsafe-inline'${isDev ? " 'unsafe-eval'" : ''};
-    style-src 'self' 'unsafe-inline';
-    img-src 'self' blob: data:;
-    font-src 'self';
-    object-src 'none';
-    base-uri 'self';
-    form-action 'self';
-    connect-src 'self' ${connectSrc};
-    frame-src 'self' https://esignet.nsr.openg2p.org;
-    frame-ancestors 'none';
-    ${isDev ? '' : 'upgrade-insecure-requests;'}
-`;
 
 const nextConfig: NextConfig = {
     output: "standalone",
     reactCompiler: true,
-    async headers() {
-        return [
-            {
-                source: '/(.*)',
-                headers: [
-                    {
-                        key: 'Content-Security-Policy',
-                        value: cspHeader.replace(/\n/g, '').trim(),
-                    },
-                ],
-            },
-        ];
-    },
     webpack: (config: any, { dev }: any) => {
         // Keep existing dev watch options for better hot reloading
         if (dev) {
