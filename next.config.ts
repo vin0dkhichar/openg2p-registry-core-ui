9ff2c96d289122a4dff2c7b6fa3542139895f282
vin0dkhichar <abbb61667@gmail.com>
2026-05-13 17:11:23 +0530

    G2P-4805 Update CSP configuration to include CSP_CONNECT_SRC from environment variables

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
 const isDev = process.env.NODE_ENV === 'development';
+const connectSrc = process.env.CSP_CONNECT_SRC || "'self'";
 
 const cspHeader = `
     default-src 'self';
@@ -15,9 +16,8 @@ const cspHeader = `
     object-src 'none';
     base-uri 'self';
     form-action 'self';
-
+    connect-src 'self' ${connectSrc};
     frame-src 'self' https://esignet.nsr.openg2p.org;
-
     frame-ancestors 'none';
     ${isDev ? '' : 'upgrade-insecure-requests;'}
 `;
